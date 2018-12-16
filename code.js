a64ec52a0667f2a2688d37826ac023d4a1e85ee4
Mrs. Haggard <cheri@MATHmaniaRobotics.com>
2018-12-16 08:36:33 -0800

    Add files to Always Remember

---
+++ code.js
@@ -0,0 +1,105 @@
+/*
+https://studio.code.org/projects/applab/1pxDXk5NM12GzNSKBMx3tA/view
+idea of slideshow with click
+photos of kids in ward being reverent, taking sacrament, bread, water, 
+Jesus' life & death
+scriptures, quotes
+
+YW Personal Progress project
+sell app or donation $1 toward mission
+*/
+
+
+
+onEvent("buttonSlideshow", "click", function() {
+  setScreen("screen1");
+});
+
+onEvent("buttonMorgan", "click", function() {
+  setScreen("screenMorgan");
+});
+
+onEvent("buttonCredits", "click", function() {
+  setScreen("screenCredits");
+});
+
+onEvent("button1Back", "click", function() {
+  setScreen("screen6");
+});
+
+onEvent("button1Next", "click", function() {
+  setScreen("screen2");
+});
+
+////
+onEvent("button1Home", "click", function() {
+  setScreen("screenSplash");
+});
+
+onEvent("button2Back", "click", function() {
+  setScreen("screen1");
+});
+
+onEvent("button2Next", "click", function() {
+  setScreen("screen3");
+});
+
+onEvent("button2Home", "click", function() {
+  setScreen("screenSplash");
+});
+
+onEvent("button3Back", "click", function() {
+  setScreen("screen2");
+});
+
+onEvent("button3Next", "click", function() {
+  setScreen("screen4");
+});
+
+onEvent("button3Home", "click", function() {
+  setScreen("screenSplash");
+});
+
+onEvent("button4Back", "click", function() {
+  setScreen("screen3");
+});
+
+onEvent("button4Next", "click", function() {
+  setScreen("screen5");
+});
+
+onEvent("button4Home", "click", function() {
+  setScreen("screenSplash");
+});
+
+onEvent("button5Back", "click", function() {
+  setScreen("screen4");
+});
+
+onEvent("button5Next", "click", function() {
+  setScreen("screen6");
+});
+
+onEvent("button5Home", "click", function() {
+  setScreen("screenSplash");
+});
+
+onEvent("button6Back", "click", function() {
+  setScreen("screen5");
+});
+
+onEvent("button6Next", "click", function() {
+  setScreen("screen1");
+});
+
+onEvent("button5Home", "click", function() {
+  setScreen("screenSplash");
+});
+
+onEvent("buttonCreditsHome", "click", function() {
+  setScreen("screenSplash");
+});
+
+onEvent("buttonMorganHome", "click", function() {
+  setScreen("screenSplash");
+});
